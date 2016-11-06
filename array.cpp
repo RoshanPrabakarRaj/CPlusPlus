3b7271bf53199ea7373f189d513ea07bc31469b6
Roshan Prabakar raj <proshan94@gmail.com>
2016-11-05 17:32:44 -0700

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a[10];
+	int i;
+	for(i=0;i<10;i++)
+	{
+	  a[i]=i+100; 
+	}
+	for(i=0;i<10;i++)
+	{
+	    cout<<a[i]<<endl;
+	}
+	return 0;
+}
